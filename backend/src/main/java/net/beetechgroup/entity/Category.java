84c44b7131c9290ddc4a98cb9f52d18f9f783a94
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2024-09-29 15:59:57 -0400

    Alterando userId para user_id

--- backend/src/main/java/net/beetechgroup/entity/Category.java
+++ backend/src/main/java/net/beetechgroup/entity/Category.java
@@ -1,9 +1,11 @@
 package net.beetechgroup.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,5 +18,8 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
+    @Column(name = "user_id")
     private String userId;
+    @Column(name = "registration_date")
+    private LocalDateTime registrationDate = LocalDateTime.now();
 }
