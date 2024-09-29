c4743795bb308042b0bc8f58425a893cf4eacab4
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2024-09-29 15:58:47 -0400

    Adicionando paginação na busca de categorias

--- backend/src/main/java/net/beetechgroup/resource/CategoriesResource.java
+++ backend/src/main/java/net/beetechgroup/resource/CategoriesResource.java
@@ -1,12 +1,16 @@
 package net.beetechgroup.resource;
 
+import io.quarkus.panache.common.Page;
+import io.quarkus.panache.common.Sort;
 import io.quarkus.security.Authenticated;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 import java.net.URI;
 import java.util.List;
@@ -36,10 +40,20 @@ public class CategoriesResource {
 
     @GET
     @Authenticated
-    public Response retrieveAll() {
+    public Response retrieveAll(
+            @QueryParam("page") @DefaultValue("0") Integer page,
+            @QueryParam("size") @DefaultValue("10") Integer size,
+            @QueryParam("orderBy") @DefaultValue("registrationDate") String orderBy,
+            @QueryParam("sortDirection") @DefaultValue("Descending") String sortDirection
+    ) {
         String email = idToken.getClaim("email");
-        List<Category> categories = this.categoryRepository.find("userId = ?1", email).stream().toList();
-        return Response.ok(categories).build();
+        List<Category> categories =
+                this.categoryRepository
+                        .find("userId = ?1", email, Sort.by(orderBy, Sort.Direction.valueOf(sortDirection)))
+                        .page(Page.of(page, size))
+                        .list();
+        long count = this.categoryRepository.count();
+        return Response.ok(new PaginatedResult<>(categories, count)).build();
     }
 
     @GET
