8350fda8584b615b2fb85eb29d8e84ab8ead0753
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2024-09-29 16:01:07 -0400

    start_time e finish_time

--- backend/src/main/java/net/beetechgroup/entity/TaskItem.java
+++ backend/src/main/java/net/beetechgroup/entity/TaskItem.java
@@ -1,5 +1,6 @@
 package net.beetechgroup.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,8 @@ public class TaskItem {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(name = "start_time")
     private LocalDateTime startTime;
+    @Column(name = "finish_time")
     private LocalDateTime finishTime;
 }
