8859d8b4e2b03477bbc1b4e4a2b9867c5fae17db
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2024-10-01 06:59:13 -0400

    removendo CustomizedInputBase

--- frontend/src/app/categories/page.tsx
+++ frontend/src/app/categories/page.tsx
@@ -1,45 +1,13 @@
 'use client';
 
-import {InputBase, Paper} from "@mui/material";
-import IconButton from '@mui/material/IconButton';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import * as React from "react";
 import {CategoriesProvider} from "@/app/contexts/CategoriesContext";
-import CreateTaskForm from "@/app/components/forms/sections/CreateTaskForm";
-
-function CustomizedInputBase() {
-  return (
-      <Paper
-          component="form"
-          sx={{
-            p: '2px 4px',
-            display: 'flex',
-            alignItems: 'center',
-            width: 400,
-            backgroundColor: '#F5F6FA',
-            borderRadius: 5,
-            border: '1px solid #E5E7EB',
-          }}
-
-      >
-        <IconButton type="button" sx={{p: '10px'}} aria-label="search">
-          <SearchOutlinedIcon/>
-        </IconButton>
-
-        <InputBase
-            sx={{ml: 1, flex: 1}}
-            placeholder="Search"
-            inputProps={{'aria-label': 'search'}}
-        />
-
-      </Paper>
-  );
-}
+import {CategoriesTable} from "@/app/components/tables/CategoriesTable";
 
 export default function CategoriesPage() {
   return (
    <CategoriesProvider>
-     <CreateTaskForm/>
+     <CategoriesTable/>
    </CategoriesProvider>
   )
 }
