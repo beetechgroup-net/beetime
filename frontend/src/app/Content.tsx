60ca2f56c800dfee98a7c8b8ce3ca8a45d97a1dc
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2024-10-01 06:57:15 -0400

    removendo code do layout e migrando para Content.tsx

---
+++ frontend/src/app/Content.tsx
@@ -0,0 +1,41 @@
+// src/app/layout/Content.tsx
+
+'use client';
+
+import { signIn, useSession } from 'next-auth/react';
+import { ReactNode } from 'react';
+import { CssBaseline, Paper, Stack, ThemeProvider } from "@mui/material";
+import defaultTheme from "@/app/themes/DefaultTheme";
+import TopBar from "@/app/layout/TopBar";
+import SideBar from "@/app/layout/side-bar/SideBar";
+import { SnackbarProvider } from 'notistack';
+
+export default function Content({ children }: { children: ReactNode }) {
+  const { status } = useSession();
+
+  if (status === "unauthenticated") {
+    void signIn("keycloak");
+    return null;
+  }
+
+  return (
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <SnackbarProvider maxSnack={5} autoHideDuration={3000}>
+          <Stack direction="row" height={"100vh"} maxHeight={"100vh"} width={"100vw"} maxWidth={"100vw"}>
+            <SideBar />
+            <Stack width={"100%"} height={"100%"} maxHeight={"100%"} maxWidth={"100%"} >
+              <TopBar />
+              <Stack padding={10} alignItems={"center"} height={"100%"}>
+                <Paper elevation={5} variant="outlined">
+                  <Stack padding={2} justifyContent={"center"} alignItems={"center"} spacing={5}>
+                    {children}
+                  </Stack>
+                </Paper>
+              </Stack>
+            </Stack>
+          </Stack>
+        </SnackbarProvider>
+      </ThemeProvider>
+  );
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,66 +1,18 @@
+// src/app/layout.tsx
+
 'use client';
 
-import {SessionProvider, signIn, useSession} from 'next-auth/react';
-import {ReactNode} from 'react';
-import {CssBaseline, Paper, Stack, ThemeProvider} from "@mui/material";
-import defaultTheme from "@/app/themes/DefaultTheme";
-import TopBar from "@/app/layout/TopBar";
-import SideBar from "@/app/layout/side-bar/SideBar";
-import {SnackbarProvider} from 'notistack';
+import { SessionProvider } from 'next-auth/react';
+import { ReactNode } from 'react';
+import Content from "@/app/Content";
 
-
-export function Content({children}: { children: ReactNode }) {
-  const {status} = useSession()
-
-  if (status === "unauthenticated") {
-    void signIn("keycloak");
-  } else {
-    return (
-        <ThemeProvider theme={defaultTheme}>
-          <CssBaseline/>
-          <SnackbarProvider maxSnack={5} autoHideDuration={3000}>
-            <Stack direction="row" height={"100vh"} maxHeight={"100vh"} width={"100vw"}
-                   maxWidth={"100vw"}>
-              <SideBar/>
-              <Stack width={"100%"} height={"100%"} maxHeight={"100%"} maxWidth={"100%"}>
-                <TopBar/>
-                <Stack
-                    padding={10}
-                    alignItems={"center"}
-                    height={"100%"}
-                >
-                  <Paper
-                      elevation={5}
-                      variant="outlined"
-                  >
-                    <Stack
-                        padding={2}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                        spacing={5}
-                    >
-                      {children}
-                    </Stack>
-                  </Paper>
-                </Stack>
-              </Stack>
-            </Stack>
-          </SnackbarProvider>
-        </ThemeProvider>
-    )
-  }
-}
-
-export default function RootLayout({children}: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
       <html lang="en">
       <body>
       <SessionProvider>
-        <Content>
-          {children}
-        </Content>
+        <Content>{children}</Content>
       </SessionProvider>
-
       </body>
       </html>
   );
